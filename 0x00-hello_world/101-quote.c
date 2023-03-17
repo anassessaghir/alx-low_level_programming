e034108167ae30e1884547344b2d3e37ef687b2b
anassessaghir <anass.essaghir@gmail.com>
2023-03-16 20:46:38 -0700

    preprocessor

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - A program that prints a string without using printf or puts.
- *
- * Return: Always 1 (Failure)
- */
+* main - print quote
+*
+* Description: print Dora's quote
+* Return: Always 1 (Success)
+*/
+
 int main(void)
 {
-	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	while (*str)
-		write(STDERR_FILENO, str++, 1);
-
-	return (1);
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
